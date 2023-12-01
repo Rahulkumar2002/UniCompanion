555e7d2d49d367bb0bc00ddfb496d9229923fed7
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2023-12-01 15:00:59 +0530

    tensorflow version lowered

--- model_flask_app/app.py
+++ model_flask_app/app.py
@@ -1,13 +1,13 @@
 import os
 import sys
 import transformers
-# import tensorflow as tf
+import tensorflow as tf
 # from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import TFAutoModelForSeq2SeqLM, DataCollatorForSeq2Seq
 from transformers import AdamWeightDecay
 from transformers import AutoTokenizer, TFAutoModelForSeq2SeqLM
-import pickle 
+# import pickle 
 from flask import Flask , request, render_template , jsonify
 from flask_cors import CORS, cross_origin
 
